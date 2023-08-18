107fe07a3fd2b085c73fdc4ddc6ff70440c8c4f4
Михаил Савенко <mi47sav@gmail.com>
2023-08-18 13:13:51 +0300

    Project 12. Review 1. 7 p7

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-settings-secret-key')
 DEBUG = os.getenv('DEBAG') == 'True'
 
 AH = os.getenv('ALLOWED_HOSTS') 
